1e35e923d908512324f6ff8f9cd9a83d0cfc16b6
Jess Xu <jsxu315@gmail.com>
2023-11-19 19:41:10 -0800

    Refactored materials, textures, shapes into their own file

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import {Particle_Shader} from "./particleshader.js";
 import { mapComponents } from './map.js';
 import { checkMapComponentCollisions } from './collisions.js';
 import * as CONST from './constants.js'
+import {Test_Data} from './materials.js'
 // Pull these names into this module's scope for convenience:
 const {vec3, unsafe3, vec4, color, Mat4, Light, Shape, Material, Shader, Texture, Scene} = tiny;
 
@@ -92,36 +93,36 @@ export class Simulation extends Scene {
 }
 
 
-export class Test_Data {
-    // **Test_Data** pre-loads some Shapes and Textures that other Scenes can borrow.
-    constructor() {
-        this.textures = {
-            rgb: new Texture("assets/rgb.jpg"),
-            earth: new Texture("assets/earth.gif"),
-            grid: new Texture("assets/stars.png"),
-            stars: new Texture("assets/stars.png"),
-            text: new Texture("assets/text.png"),
-        }
-        this.shapes = {
-            donut: new defs.Torus(15, 15, [[0, 2], [0, 1]]),
-            cone: new defs.Closed_Cone(4, 10, [[0, 2], [0, 1]]),
-            capped: new defs.Capped_Cylinder(4, 12, [[0, 2], [0, 1]]),
-            ball: new defs.Subdivision_Sphere(3, [[0, 1], [0, 1]]),
-            cube: new defs.Cube(),
-            prism: new (defs.Capped_Cylinder.prototype.make_flat_shaded_version())(10, 10, [[0, 2], [0, 1]]),
-            gem: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
-            donut2: new (defs.Torus.prototype.make_flat_shaded_version())(20, 20, [[0, 2], [0, 1]]),
-
-            snowball2: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(3),
-        };
-    }
-
-    random_shape(shape_list = this.shapes) {
-        // random_shape():  Extract a random shape from this.shapes.
-        const shape_names = Object.keys(shape_list);
-        return shape_list[shape_names[~~(shape_names.length * Math.random())]]
-    }
-}
+// export class Test_Data {
+//     // **Test_Data** pre-loads some Shapes and Textures that other Scenes can borrow.
+//     constructor() {
+//         this.textures = {
+//             rgb: new Texture("assets/rgb.jpg"),
+//             earth: new Texture("assets/earth.gif"),
+//             grid: new Texture("assets/stars.png"),
+//             stars: new Texture("assets/stars.png"),
+//             text: new Texture("assets/text.png"),
+//         }
+//         this.shapes = {
+//             donut: new defs.Torus(15, 15, [[0, 2], [0, 1]]),
+//             cone: new defs.Closed_Cone(4, 10, [[0, 2], [0, 1]]),
+//             capped: new defs.Capped_Cylinder(4, 12, [[0, 2], [0, 1]]),
+//             ball: new defs.Subdivision_Sphere(3, [[0, 1], [0, 1]]),
+//             cube: new defs.Cube(),
+//             prism: new (defs.Capped_Cylinder.prototype.make_flat_shaded_version())(10, 10, [[0, 2], [0, 1]]),
+//             gem: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
+//             donut2: new (defs.Torus.prototype.make_flat_shaded_version())(20, 20, [[0, 2], [0, 1]]),
+//
+//             snowball2: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(3),
+//         };
+//     }
+//
+//     random_shape(shape_list = this.shapes) {
+//         // random_shape():  Extract a random shape from this.shapes.
+//         const shape_names = Object.keys(shape_list);
+//         return shape_list[shape_names[~~(shape_names.length * Math.random())]]
+//     }
+// }
 
 export class Main_Demo extends Simulation {
     // ** Inertia_Demo** demonstration: This scene lets random initial momentums
@@ -132,28 +133,31 @@ export class Main_Demo extends Simulation {
         this.shapes = Object.assign({}, this.data.shapes);
         this.shapes.square = new defs.Square();
         const shader = new defs.Fake_Bump_Map(1);
-        this.material = new Material(shader, {
-            color: color(.4, .8, .4, 1),
-            ambient: .4, texture: this.data.textures.stars
-        })
-        this.snowballMtl = new Material(new defs.Phong_Shader(), {
-            color: color(1, 1, 1, 1),
-            ambient: 0.8, 
-        })
 
-        this.snowballExplosionMtl = new Material(new Particle_Shader(), {
-            color: color(1, 1, 1, 1),
-            ambient: 0.8,
-            localTime: 0.0,
-        })
+        this.materials = Object.assign({}, this.data.materials);
 
-
-
-        this.inactive_color = new Material(shader, {
-            color: color(.5, .5, .5, 1), ambient: .2,
-            texture: this.data.textures.rgb
-        });
-        this.active_color = this.inactive_color.override({color: color(.5, 0, 0, 1), ambient: .5});
+        // this.material = new Material(shader, {
+        //     color: color(.4, .8, .4, 1),
+        //     ambient: .4, texture: this.data.textures.stars
+        // })
+        // this.snowballMtl = new Material(new defs.Phong_Shader(), {
+        //     color: color(1, 1, 1, 1),
+        //     ambient: 0.8,
+        // })
+        //
+        // this.snowballExplosionMtl = new Material(new Particle_Shader(), {
+        //     color: color(1, 1, 1, 1),
+        //     ambient: 0.8,
+        //     localTime: 0.0,
+        // })
+        //
+        //
+        //
+        // this.inactive_color = new Material(shader, {
+        //     color: color(.5, .5, .5, 1), ambient: .2,
+        //     texture: this.data.textures.rgb
+        // });
+        // this.active_color = this.inactive_color.override({color: color(.5, 0, 0, 1), ambient: .5});
 
         this.colliders = [
             {intersect_test: Body.intersect_sphere, points: new defs.Subdivision_Sphere(1), leeway: .5},
@@ -282,7 +286,7 @@ export class Main_Demo extends Simulation {
                     // this.data.shapes.snowball2,
                     //  this.snowballMtl,
                     this.data.shapes.ball, 
-                    this.snowballMtl,
+                    this.materials.snowballMtl,
 
                     vec3(0.7, 0.7, 0.7),
                     this.player.getPlayerID()
@@ -391,7 +395,7 @@ export class Main_Demo extends Simulation {
             Mat4.translation(0, -2, 0)
                 .times(Mat4.rotation(Math.PI / 2, 1, 0, 0))
                 .times(Mat4.scale(50, 50, 1)),
-            this.material.override(this.data.textures.earth)
+            this.materials.material.override(this.data.textures.earth)
         )
         // border walls
         // for (let i = 0; i < 0; i++) {
@@ -417,7 +421,7 @@ export class Main_Demo extends Simulation {
                 Mat4.translation(...piece.translate)
                     .times(Mat4.rotation(piece.roationAngle, ...piece.rotation))
                     .times(Mat4.scale(...piece.scale)),
-                this.material
+                this.materials.material
             )
         }
 
